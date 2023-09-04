fcfe0265e98b380ae2d4bf7122756d43b069186e
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 16:16:21 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+
+
+
+function removeDuplicates(arr) {
+  const uniqueArray = [];
+
+  for (let item of arr) {
+    if (!uniqueArray.includes(item)) {
+      uniqueArray.push(item);
+    }
+  }
+
+  return uniqueArray;
+}
+
+// Example usages:
+console.log(removeDuplicates([1, 0, 1, 0])); // Output: [1, 0]
+console.log(removeDuplicates(["New", "York", "City"])); // Output: ["New", "York", "City"]
+console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"])); // Output: ["Hassan", "Aisha"]
+
